94116921793fcee100fcaefbc4e090a6f18a9c8f
hidelbrandorios <hidels.rios@gmail.com>
2024-03-12 10:01:58 -0300

    start project

---
+++ coffee-delivery/src/styles/theme/theme.ts
@@ -0,0 +1,46 @@
+export const theme = {
+  color: {
+    "yellow-dark": "#C47F17",
+    "yellow": "#DBAC2C",
+    "yellow-light": "#F1E9C9",
+
+    "purple-dark": "#4B2995",
+    "purple": "#8047F8",
+    "purple-light": "#EBE5F9",
+
+    "base-title": "#272221",
+    "base-subtitle": "#403937",
+    "base-text": "#574F4D",
+    "base-label": "#8D8686",
+    "base-hover": "#D7D5D5",
+    "base-button": "#E6E5E5",
+    "base-input": "#EDEDED",
+    "base-card": "#F3F2F2",
+
+    "background": "#FAFAFA",
+    "white": "#FFFFFF",
+  },
+
+  font: {
+    "font-family": {
+      "balloon-2": "'Balloon 2', sans-serif",
+      "roboto": "'Roboto', sans-serif",
+    },
+    "font-size": {
+      "titleXL": "48px",
+      "titleL": "32px",
+      "titleM": "24px",
+      "titleS": "20px",
+      "titleXS": "18px",
+
+      "textL": "20px",
+      "textM": "16px",
+      "textS": "14px",
+      "textXS": "12px",
+
+      "tag": "10px",
+      "buttonG": "14px",
+      "buttonM": "12px",
+    },
+  },
+};
